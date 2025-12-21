f7eb1d0b8158706392543bc8efc2c9de887c19a8
Reergard <oleksandr95kryvoruchko@gmail.com>
2025-12-21 17:17:00 +0100

    Страница приглашения

--- wedding_OK/urls.py
+++ wedding_OK/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
